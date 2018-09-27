bad3b5a30d9ec88859c9bae9a77cd8a9b7a1fbbd
juvas array <achouraithamiche40@gmail.com>
2018-09-27 21:03:30 +0100

    Changes

--- vuebnb-prototype/app.js
+++ vuebnb-prototype/app.js
@@ -1,8 +1,11 @@
 var app = new Vue({
     el: "#app",
     data: {
-        title: "My appartement",   
-        address : "12, Village Tafraout, Ait Yahia, Tizi Ouzou",
-        description: "Cette maison du style Kabyle est une trés belle maison qui vous offre tout le confort nécessaire pour passer d'agréables moments avec votre famille."
+        title: sample.title,
+        address : sample.address,
+        about: sample.about,
+        headerImageStyle: {
+            'background-image': 'url(sample/header.jpg)',
+        }
     },
 })
